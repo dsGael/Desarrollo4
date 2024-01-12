a0cdd193bebe7735e628856d6e58be55bb386063
Gael Borchardt <117969020+dsGael@users.noreply.github.com>
2024-01-12 14:44:45 -0700

    Update diccionarios.py

--- diccionarios.py
+++ diccionarios.py
@@ -25,4 +25,40 @@ except KeyError:
     print('No existe la carrera de civil')    
 
     
+facultad['civil']={}
+facultad['civil']['IC']='Ingenieria Civil'
 
+if 'civil' in facultad:
+    print(facultad['civil'])
+
+print("\n----------------------")
+
+cadena="el caballo corre por el campo"
+diccionario={}
+for letra in cadena:
+    if letra in diccionario:
+        diccionario[letra]+=1
+    else:
+        diccionario[letra]=1
+
+print(diccionario)
+
+for k,v in diccionario.items():
+    print(k,v)
+
+print("\n----------------------")
+diccionario_ordenado= sorted(diccionario.items(),key=lambda item: item[1],reverse=True)
+diccionario_ordenado=dict(diccionario_ordenado)
+print(diccionario_ordenado)
+
+for k,v in diccionario_ordenado.items():
+    print(f"K:{k} V: {v}")
+
+
+diccionario_ordenado= dict(sorted(diccionario.items(),key=lambda item: item[0],reverse=False))
+
+for k,v in diccionario_ordenado.items():
+    print(f"K:{k}  \tV: {v}")
+
+
+    
