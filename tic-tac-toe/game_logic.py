5502f33ac96ac585e31e9df817360ee5ebc37b53
Gael Borchardt <117969020+dsGael@users.noreply.github.com>
2024-01-26 14:52:44 -0700

    Update game_logic.py

--- tic-tac-toe/game_logic.py
+++ tic-tac-toe/game_logic.py
@@ -1,4 +1,7 @@
 import random 
+import board
+
+
 '''
 logica del programa del gato
 '''
@@ -42,39 +45,30 @@ def juega_usuario(tab):
 def game(tab:dict):
     turnos=0
     diccionario={'ganador':''}
-    
     while turnos <8:
-        display_tablero(tab)
+        board.display_tablero(tab)
         correcto=juega_usuario(tab)
         if correcto:
             turnos+=1
             gana=win(tab)
-        
             if gana:
                 diccionario["ganador"]="Jugador"
-                display_tablero(tab)
+                board.display_tablero(tab)
                 print("Ganaste!")
-                
                 break
             IA(tab)  
             gana=win(tab)
-        
-
             if gana:
                 diccionario["ganador"]="IA"
-                display_tablero(tab)
+               board.display_tablero(tab)
                 print("Gana IA")
-                
-                break
-                
-            
+                break              
             turnos+=1          
-    display_tablero(tab)
+    board.display_tablero(tab)
     return diccionario
         
 
 def win(tab):
-
     lista_lineas=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     for comb in lista_lineas:
         if tab[comb[0]]==tab[comb[1]]==tab[comb[2]]:
@@ -82,34 +76,46 @@ def win(tab):
     return False
 
 
-def game_cycle(tab):
-    
-    d=game(tab)
-    if d['ganador']!="":
-        print(f"Ganador: {d['ganador']}")
-        score[d['ganador']]+=1   
-    else:
-        print("Empate")
-        score['Empates']+=1
-    print(f"Marcador: {score}")
-        
-
-if __name__ == "__main__":
+def game_cycle():
     opcion=1
     continuar=True
     score={"Jugador":0,"IA":0, "Empates":0}
     while continuar:
-
         tab=reset_tablero()
         print("TIC TAC TOE\n")
+        d=game(tab)
+        display_score(score,d)
+        continuar = play_again()
+   
+        
+def display_score(s:dict,d:dict):
+    '''
+    s= diccionario de scores keys
+    d= diccionario cobn el ganador del juego anterior key
+    '''
+    if d['ganador']!="":
+        print(f"Ganó: {d['ganador']}")
+        s[d['ganador']]+=1   
+    else:
+        print("¡Empate!")
+        s['Empates']+=1
+    print(f"<<Jugador:{s['Jugador']}>> <<IA:{s['IA']}>> <<Empates:{s['Empates']}>>")
 
-        game_cycle(tab)
 
-        print("¿Otra partida?")
+def play_again():
+    continuar=True
+    print("¿Otra partida?")
         print("1 >> Si")
         print("2 >> No")
         opcion=input()
         if opcion!="1":
             continuar=False
             print("bai bai")
+    return continuar
+
+if __name__ == "__main__":
+   game_cycle()
+    
+        
+        
                 
