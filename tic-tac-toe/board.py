5adcabbac74f63df2d1b96b57cb37b6cdff6c112
Gael Borchardt <117969020+dsGael@users.noreply.github.com>
2024-01-29 14:42:34 -0700

    Update board.py

--- tic-tac-toe/board.py
+++ tic-tac-toe/board.py
@@ -2,14 +2,14 @@ from colorama import Fore,Back,Style,Cursor
 
 def display_tablero(tablero:dict):
     reset= Style.RESET_ALL
-    bg=Back.WHITE
+    bg=Back.BLACK
     blue=Fore.BLUE
     board_color = Fore.LIGHTCYAN_EX
     x_color =Fore.RED
-    o_color=Fore.GREEN
+    o_color=Fore.LIGHTGREEN_EX
     X= x_color+"X"
     O = o_color+"O"
-    BD=board_color+"----------"
+    BD=board_color+"----------- "
     BS=board_color+" | "
     d={}
     for k,v in tablero.items():
@@ -20,10 +20,11 @@ def display_tablero(tablero:dict):
         else:
             d[k]= blue + str(k) + BS
     Cursor.POS(10,5)
-    print(f"{bg}{BD}{reset}")
-    print(f"{bg}|{d[0]}{d[1]}{d[2]}{reset}")
-    print(f"{bg}{BD}{reset}")
-    print(f"{bg}|{d[3]}{d[4]}{d[5]}{reset}")
-    print(f"{bg}{BD}{reset}")
-    print(f"{bg}|{d[6]}{d[7]}{d[8]}{reset}")
+  
+    
+    print(Cursor.POS(20,5)+f"{bg}{d[0]}{d[1]}{d[2]}{reset}")
+    print(Cursor.POS(20,6)+f"{bg}{BD}{reset}")
+    print(Cursor.POS(20,7)+f"{bg}{d[3]}{d[4]}{d[5]}{reset}")
+    print(Cursor.POS(20,8)+f"{bg}{BD}{reset}")
+    print(Cursor.POS(20,9)+f"{bg}{d[6]}{d[7]}{d[8]}{reset}")
     print(Style.RESET_ALL)
