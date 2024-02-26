00bc54fde928405108134fd1995911c6574889db
gael <gael@gmail.com>
2024-02-26 14:52:39 -0700

    actualizado catalogo/catalogos.py ```

--- catalogo/catalogos.py
+++ catalogo/catalogos.py
@@ -1,6 +1,6 @@
 import csv
-import os
-import argparse 
+import os 
+import argparse
 
 
 class Revista:
@@ -26,20 +26,61 @@ def read_csv(file_path:str) -> list:
         reader = csv.reader(csvfile)
         return [row for row in reader]
 
-def main(folder:str):
-    files_list=read_folder(folder)
-    files_list=[file for file in files_list if file.endswith(".csv")]
-    catalogo_list=extract_left_of_underscore(files_list)
+def main(folder:str, search_keys:list):
+    files_list = read_folder(folder)
+    files_list = [file for file in files_list if file.endswith('.csv')]
+    catalogo_list = extract_left_of_underscore(files_list)
     print(catalogo_list)
-    lista_revistas=[]
+    lista_revistas = []
+    diccionario = {}
     for files in files_list:
-        titulos=read_csv(files)
-        lista_revistas.append(titulos)
+        filename = os.path.join(folder,files)
+        titulos = read_csv(filename) #leyendo los titulos
+        catalogo = files.split("_")[0] #extraemos nombre del catalogo
+        for titulo in titulos:
+            titulo = titulo[0]
+            titulo = titulo.lower()
+            revista = Revista(titulo,catalogo)
+            #agregar revista al diccionario
+            agregar_revista_diccionario(diccionario,revista)
+    #print(f"Número de llaves en diccionario:{len(diccionario.keys())}")
+    #print(f"Revista 'acta':",diccionario['acta'])
+    lista=search_keywords(diccionario,search_keys)
+    print(f"Resultado:")
+    for revista in lista:
+        print(revista)
 
-    
+
+def agregar_revista_diccionario(dic:dict,revista:Revista):
+    titulo = revista.titulo
+    lista_palabras = titulo.split(" ")
+    lista_palabras.append(titulo)
+    for palabra in lista_palabras:
+        if palabra not in dic:
+            dic[palabra] = [revista]
+        else:
+            dic[palabra].append(revista)
+
+def search_keywords(dictionary:dict,keys:list):
+    values= [set(dictionary.get(key,[])) for key in keys]
+    if not values:
+        return []
+    intersection =values[0]
+    for value in values[1:]:
+        intersection &=value
+    return list(intersection)
+        
+
 if __name__ == "__main__":
-    parser=argparse.ArgumentParser(description="Procesa archivos csv y genera catalogo")
-    parser.add_argument('folder_path', type=str, help="Ruta de la carpeta con archivos csv")
-    args=parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Procesa archivos csv y genera catálogo'
+    )
+    parser.add_argument('folder_path',
+                        type=str,
+                        help='Ruta de la carpeta con csvs'
+                        )
+    parser.add_argument("search_keys",metavar="K",type=str,nargs="+",)
+    args = parser.parse_args()
     folder_path = args.folder_path
-    print(folder_path)      
+    keys=args.search_keys
+    main(folder_path, keys)
