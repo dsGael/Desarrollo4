deb13d1b51daa4010a31e4b0e620ef50ae7e6680
gael <gael>
2024-03-04 14:51:41 -0700

    aaa

--- dolar/scrapper.py
+++ dolar/scrapper.py
@@ -11,10 +11,30 @@ def scrap(URL:str):
     page = requests.get(URL)
     return page
 
+def get_exchange_rate(dom):
+    exchange_rates={}
+    for row in dom.find_all('p'):
+        print(f"{row}")
+        title=row.text.strip()
+        if title[0] == 'C':
+            title='Compra'
+        elif title[0] == 'V':
+            title='Venta'
+        
+        value=row.find('span')
+        value=value.text.strip()
+        exchange_rates[title]=value
+
+    return exchange_rates
+
+
 def main():
     url='https://bit.ly/dolarInfo'
     pagina=scrap(url)
-    print(pagina.content)
+    soup=BeautifulSoup(pagina.content, 'html.parser')
+    results=soup.find(class_="exchangeRate")
+    ex=get_exchange_rate(results)
+    print(ex)
 
 
 if __name__ == '__main__':
