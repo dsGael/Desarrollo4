0902f92bf1143e8373f5ab5175dc0a5eb3a81fa0
gael <gael>
2024-03-22 14:46:47 -0700

    cambios

--- Ahorcado/app.py
+++ Ahorcado/app.py
@@ -5,21 +5,37 @@ import random
 
 
 app=Flask(__name__)
-
-palabras=lee_archivo("Ahorcado/palabras.txt") 
 conteo=0
-abc="ABCDEFGHIJKLMNOPQSTUVWXYZ"
+palabras=lee_archivo("Ahorcado/palabras.txt") 
+abc="abcdefghijklmnopqrstuvwxyz"
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    global conteo
+    global palabraL
+    global listado
+     
     if request.method=='GET':
         palabra=random.choice(palabras)
+        palabraL=palabra_toList(palabra)
         image=f"/static/images/monito-{conteo}.png"
         letras=[{'letra':x, 'id_letra':x}for x in abc]
         listado=[(d['letra'],d['id_letra']) for d in letras]
-        palabraL=palabra_toList(palabra)
         return render_template('index.html', imagen=image, abecedario=abc, lista_abc=listado, lista_pal=palabraL)
 
+    if request.method=='POST':
+        existe=False
+        valor=request.form['valor']
+        for dic in palabraL:
+            if valor in dic: 
+                dic[valor]=valor
+                existe=True
+        if not existe:
+            conteo=+1 
+
+        image=f"/static/images/monito-{conteo}.png"
+        return render_template('index.html', imagen=image, abecedario=abc, lista_abc=listado, lista_pal=palabraL)
+        
 
     
 if __name__=='__main__':
