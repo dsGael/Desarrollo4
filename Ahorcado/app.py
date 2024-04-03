15a235de2324b7d415423ddff2ca7c04fa36c1ba
gael <gael>
2024-04-03 14:39:25 -0700

    fran

--- Ahorcado/app.py
+++ Ahorcado/app.py
@@ -1,55 +1,70 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request,redirect
 from os import path
-from funciones import lee_archivo, palabra_toList, ganador
-import random
+from funciones import lee_archivo, palabra_a_diccionario, checa_si_gano
+import random 
+app = Flask(__name__)
 
+palabras = lee_archivo("Ahorcado/palabras.txt")
+palabra = random.choice(palabras)
+lista_dict = palabra_a_diccionario(palabra)
+conteo = 0
 
-app=Flask(__name__)
-conteo=0
-palabras=lee_archivo("Ahorcado/palabras.txt") 
-abc="abcdefghijklmnopqrstuvwxyz"
-letras=[x for x in abc]
-
+abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letras = [ x.lower() for x in abc]
 @app.route('/', methods=['GET','POST'])
 def index():
     global conteo
-    global palabraList
-    global listado
-    global palabra
-    
-    global chequeo
-     
-    if request.method=='GET':
-        palabra=random.choice(palabras)
-        palabraList=palabra_toList(palabra)
-        image=f"/static/images/monito-{conteo}.png"
-        Lista_letras=[{'letra':x, 'id_letra':x}for x in abc]
-        listado=[(d['letra'],d['id_letra']) for d in Lista_letras]
-        return render_template('index.html', imagen=image, abecedario=abc, lista_abc=listado, lista_pal=palabraList)
-   
-    if request.method=='POST':
-        chequeo=False
-        existe=False
-        valor=request.form['valor']
-        for dic in palabraList:
-            if valor in dic: 
-                dic[valor]=valor
-                existe=True
-                chequeo=ganador(palabraList)
-                image=f"/static/images/monito-{conteo}.png"
-               
-                    
-                    
-        if not existe:
-            conteo+=1 
-            image=f"/static/images/monito-{conteo}.png"
-            
-           
-      
-        
-        return render_template('index.html', imagen=image, abecedario=abc, lista_abc=listado, lista_pal=palabraList ,conteo=conteo,chequeo=chequeo)
-        
+    global lista_dict
+    global abc
+    global fin
 
+    if request.method == 'GET':
+        fin=False
+        image = f"/static/images/monito-{conteo}.png"
+        string_abc = "".join(letras)
+        lista_letras = [ {'letra':x, 'id_letra':x} for x in string_abc]
+        listado = [(d['letra'],d['id_letra']) for d in lista_letras]
+        return render_template("index.html", imagen=image, lista_abc=listado, abcedario=string_abc, lista_pal=lista_dict, fin_juego=fin)
     
-if __name__=='__main__':
+    if request.method == 'POST':
+        fin=False
+        gana = False
+        valor = request.form['valor']
+        valor = valor.lower()
+        existe = False
+        for diccionario in lista_dict:
+            if valor in diccionario:
+                diccionario[valor] = valor
+                existe = True
+        if existe == False:
+            conteo +=1
+        gana = checa_si_gano(lista_dict)
+        if gana == True:
+            fin = True
+        if conteo == 6:
+            fin = True
+        letras.remove(valor)
+        image = f"/static/images/monito-{conteo}.png"
+        string_abc = "".join(letras)
+        lista_letras = [ {'letra':x, 'id_letra':x} for x in string_abc]
+        listado = [(d['letra'],d['id_letra']) for d in lista_letras]
+        return render_template("index.html", imagen=image, abcedario=string_abc,lista_abc=listado,lista_pal=lista_dict, fin_juego=fin)
+
+@app.route('/', methods=['GET'])
+def nuevo_juego():
+    
+    global conteo
+    global lista_dict
+    global palabra
+    palabra = random.choice(palabras)
+    lista_dict = palabra_a_diccionario(palabra)
+    conteo = 0
+    
+    image = f"/static/images/monito-{conteo}.png"
+    string_abc = "".join(letras)
+    lista_letras = [ {'letra':x, 'id_letra':x} for x in string_abc]
+    listado = [(d['letra'],d['id_letra']) for d in lista_letras]
+    return redirect('/')
+    
+if __name__ == "__main__":
     app.run(debug=True)
--- Ahorcado/funciones.py
+++ Ahorcado/funciones.py
@@ -1,26 +1,41 @@
+import random
+
 def lee_archivo(archivo:str):
-    palabras=[]
-    data=[]
-    with open(archivo, 'r', encoding="utf-8") as file:
-        data = file.readlines()
-        for palabra in data:
-            palabra=palabra.strip("\n")
-            palabras.append(palabra)
+    palabras = []
+    with open(archivo,"r",encoding="utf-8") as a:
+        data = a.readlines()
+    for palabra in data:
+        palabra = palabra.strip("\n")
+        palabras.append(palabra)
     return palabras
 
-def palabra_toList(palabra:str)-> list:
-    lista=[{letra:"_"} for letra in palabra]
+def palabra_a_diccionario(palabra:str)->list:
+    ''' convierte palabra en un diccionario de letras'''
+    lista = [ {letra:"_"} for letra in palabra ]
     return lista
 
-def ganador(list_letras:list):
-    for dic in list_letras:
-        for k,v in dic.items():
-            if k==v:
-                return True
-            else:
-                return False
+def checa_si_gano(lista_letras:list):
+    '''[ {'g':'g'},{'a':'a'},{'t':'t'},{'o':'_'}]
+    '''
+    existe_underscore=False
+    for diccionario in lista_letras:
+        for k,v in diccionario.items():
+            if v == '_':
+                existe_underscore=True
+    if existe_underscore==True:
+        gana = False
+    else:
+        gana = True
+    return gana
+
 
 if __name__ == "__main__":
-    print(lee_archivo("ahorcado/palabras.txt"))
-    print(palabra_toList("papu"))
-    
+    palabras = lee_archivo("palabras.txt")
+    print(palabras)
+    palabra = random.choice(palabras)
+    dp = palabra_a_diccionario(palabra)
+    print(dp)
+    gat_ = [ {'g':'g'},{'a':'a'},{'t':'t'},{'o':'_'}]
+    gato = [ {'g':'g'},{'a':'a'},{'t':'t'},{'o':'o'}]
+    print(checa_si_gano(gat_))
+    print(checa_si_gano(gato))
