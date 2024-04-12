411bdd902b34332418a7a4b57605cf6ebc02404f
gael <gael>
2024-04-12 14:50:31 -0700

    genero/<id>

--- cartelera/app.py
+++ cartelera/app.py
@@ -1,36 +1,59 @@
+import unicodedata
 from flask import Flask, render_template, request
-from funciones import carga_csv, peliculas_mas_recientes, crea_diccionario_peliculas
+from funciones import carga_csv, peliculas_mas_recientes
+from funciones import crea_diccionario_peliculas
+from funciones import crea_diccionario_genero
+import os
 
+archivo_cartelera = 'cartelera/cartelera_2024.csv'
 app = Flask(__name__)
-cartelera=carga_csv('cartelera/cartelera_2024.csv')
+cartelera = carga_csv(archivo_cartelera)
+diccionario_peliculas=crea_diccionario_peliculas(cartelera)
+diccionario_generos = crea_diccionario_genero(cartelera)
 
-@app.route('/')
+@app.route("/")
 def index():
     global cartelera
     lista_peliculas = peliculas_mas_recientes(cartelera)
-    return render_template('indice.html', lista=lista_peliculas)
+    return render_template("index.html",lista=lista_peliculas)
 
-@app.route('/generos')
+@app.route("/generos")
 def generos():
-    return render_template('generos.html')
+    return render_template("generos.html",dicc_generos=diccionario_generos)
 
-@app.route('/anio')
+@app.route("/genero/<id>")
+def generos_id(id:str):
+    peliculas_por_genero = []
+    if id in diccionario_generos:
+        for pelicula in cartelera:
+            keys = pelicula["genero"]
+            keys = unicodedata.normalize('NFD', keys).encode('ascii', 'ignore').decode('utf-8') #quita acentos
+            keys = keys.upper()
+            keys = keys.split(",")
+            for key in keys:
+                key = key.strip()
+                if key == id:
+                    peliculas_por_genero.append(pelicula)
+    print(peliculas_por_genero)
+    return render_template("genero.html",genero=id,peliculas_por_genero=peliculas_por_genero)
+    
+
+@app.route("/anio")
 def anio():
-    return render_template('anio.html')
+    return render_template("anio.html")
 
-@app.route('/alfabetico')
+@app.route("/alfabetico")
 def alfabetico():
-    return render_template('alfabetico.html')
+    return render_template("alfabetico.html")
 
-@app.route('/pelicula/<id>')
+@app.route("/pelicula/<id>")
 def pelicula(id:str):
-    diccionario_peliculas=crea_diccionario_peliculas(cartelera)
     if id in diccionario_peliculas:
-        pelicula=diccionario_peliculas[id]
-        return render_template('movie.html', movie=pelicula)
+        pelicula = diccionario_peliculas[id]  
+        print(f"movie={pelicula['titulo']}")  
+        return render_template("movie.html",movie=pelicula)
     else:
-        return render_template('no_existe.html')
-
-
-if __name__ == '__main__':
+        return render_template("no_existe.html")
+    
+if __name__ == "__main__":
     app.run(debug=True)
--- cartelera/funciones.py
+++ cartelera/funciones.py
@@ -1,43 +1,72 @@
 import csv
 from datetime import datetime
+import unicodedata
 
-def carga_csv(archivo:str) ->list :
-    """
-    Cargar el csv y regresar una lista
-    """
+def carga_csv(nombre_archivo:str)->list:
+    '''
+    Carga archivo csv y regresa una lista 
+    '''
     lista = []
-    with open(archivo,'r',encoding='utf-8') as csv_file:
-        lista = list(csv.DictReader(csv_file))
-        return lista
+    with open(nombre_archivo,'r',encoding="utf-8") as archivo:
+        lista = list(csv.DictReader(archivo))
+    return lista
 
-def peliculas_mas_recientes(lista_peliculas:list) -> list:
-    """
-    Regresa una lista con las peliculas más recientes
-    """
-    
-    hoy=datetime.now()
+def peliculas_mas_recientes(lista_peliculas:list)->list:
+    '''Regresa las películas de más reciente estreno'''
+    listap = []
+    hoy = datetime.now()
     for pelicula in lista_peliculas:
-        estreno=pelicula['fecha_estreno']
-        estreno=datetime.strptime(estreno,'%Y/%m/%d')
-        diferencia=hoy-estreno
-        pelicula['dias_desde_estreno']=diferencia.days
-    lista_peliculas.sort(key=lambda x:x['dias_desde_estreno'])
-    sub_lista=lista_peliculas[:5]
-    return sub_lista
+        #print(pelicula['titulo']," - ",pelicula['fecha_estreno'])
+        estreno = pelicula['fecha_estreno']
+        estreno = datetime.strptime(estreno,"%Y/%m/%d")    
+        diferencia = hoy - estreno
+        pelicula['dias_desde_estreno'] = diferencia.days
+    lista_sorted = sorted(lista_peliculas, key=lambda x: x['dias_desde_estreno'],reverse=False)
+    listap = lista_sorted[:5]
+    return listap
 
 def crea_diccionario_peliculas(lista_peliculas:list)->dict:
-    d={}
+    ''' Crea diccionario de peliculas a partir de 
+        la lista de peliculas
+        {"id_pelicula" ={diccionario_pelicula}}
+    '''
+    d = {}
     for pelicula in lista_peliculas:
-       k= pelicula['id']
-       d[k]=pelicula
+        key = pelicula["id"]
+        d[key] = pelicula # key,value
     return d
 
+def crea_diccionario_genero(lista_peliculas:list)->dict:
+    ''' Crea diccionario de generos a partir de 
+        la lista de peliculas
+        {"genero" =[lista_peliculas]}
+    '''
+    d = {}
+    for pelicula in lista_peliculas:
+        keys = pelicula["genero"]
+        keys = unicodedata.normalize('NFD', keys).encode('ascii', 'ignore').decode('utf-8') #quita acentos
+        keys = keys.upper()
+        keys = keys.split(",")
+        for key in keys:
+            key = key.strip()
+            if key in d:
+                d[key].append(pelicula)
+            else:
+                d[key] = [pelicula]
+    d = {k: v for k, v in sorted(d.items(), key=lambda item: item[0])}
+    return d
 
+if __name__ == "__main__":
+    lista =carga_csv("cartelera/cartelera_2024.csv")
+    #lista =carga_csv("cartelera_estrenos.csv")
+    #print(lista)
+    print("..........")
+    lista5 = peliculas_mas_recientes(lista)
+    for pelicula in lista5:
+        print(f"{pelicula['titulo']} - {pelicula['fecha_estreno']} {pelicula['dias_desde_estreno']}")
+    d = crea_diccionario_peliculas(lista)
+    print(f"Dune 2: {d['Dune2']}")
 
-if __name__=="__main__":
-    lista = carga_csv('cartelera/cartelera_2024.csv')
-    #recientes=peliculas_mas_recientes(lista)
-  #  for pelicula in recientes:
-   #     print(pelicula['titulo'],pelicula['fecha_estreno'],pelicula['dias_desde_estreno'])
-    diccionario=crea_diccionario_peliculas(lista)
-    print(diccionario['Dune2'])
+    d = crea_diccionario_genero(lista)
+    for genero,lista in d.items():
+        print(f"Genero:{genero} | Peliculas:{len(lista)}")
